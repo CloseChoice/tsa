1a714ea7445c4359d512ecb4ed1c1540e78ee280
Tobias Pitters <tobias.pitters@gmail.com>
2020-01-22 01:24:38 +0100

    added peaks_by_rule into timeseries_utils

--- sfc/modules/utils/timeseries_utils.py
+++ sfc/modules/utils/timeseries_utils.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
 from functools import reduce
-from typing import List
+from typing import List, Union
+
 from sfc.modules.utils.Enums import Interval
 
 
@@ -71,7 +72,8 @@ def build_ts_from_gaussian_ts(periods, variances):
     be extended in the future, so that arbitrary distributions can be sampled but this shall be enough as of
     November 2019."""
 
-# Use following generating functions
+# USE FOLLOWING GENERATING FUNCTIONS
+
 
 def ts_gaussian_seasonality(x, seasonality, noise=0.01, spread_factor=0.001):
     """ time series with a gaussian seasonality, which means that each seaonsality time window represents a gaussian
@@ -106,3 +108,32 @@ def agg_ts_from_dists(start_dt: str, end_dt: str, seasonalities: List[int], time
         ts_list.append(create_ts_from_dist(start_dt, end_dt, time_interval, seasonality, dist))
     ts = reduce(lambda x, y: add_ts_with_different_dates(x, y), ts_list)  # add time series together
     return ts
+
+
+def peaks_by_rules(date_index: pd.DatetimeIndex, ts_value: Union[int, float], col_name: str, rule: str = 'day',
+                   rule_value: Union[int, float, pd.DateOffset] = 1):
+    """This function is capable of generating timeseries with non-zero values 'ts_value' if the rule is fulfilled
+    and zero values at any other time.
+    Allowed rules:
+    - any of the attributes of pd.DateIndex
+    - offsets
+    - last_n_day_of_month
+
+    Here some help in order you forget how to use the rules:
+    - pd.DateOffset(month=1) will be just the january
+    - more to come"""
+
+    def last_n_day_of_month(time_stamp):
+        return time_stamp == (time_stamp - pd.offsets.Day(1) + pd.offsets.MonthEnd()) - pd.offsets.Day(rule_value)
+
+    if rule == 'last_n_day_of_month':
+        assert 0 <= rule_value <= 31, f"rule_value out of range. Allowed values are between 0 and 31 but {rule_value} was given"
+        values = np.where(pd.Series(date_index).apply(lambda x: last_n_day_of_month(x)), ts_value, 0)
+    elif hasattr(date_index, rule):
+        values = np.where(getattr(date_index, rule) == rule_value, ts_value, 0)
+    elif isinstance(rule_value, pd.DateOffset) and rule == 'offsets':
+        values = np.where(date_index + rule_value == date_index, ts_value, 0)
+    else:
+        raise NotImplementedError(f'your rule is not implemented. Value:{rule_value}, rule: {rule}')
+    df = pd.DataFrame(values, columns=[col_name], index=date_index)
+    return df
